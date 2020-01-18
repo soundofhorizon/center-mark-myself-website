492d18ba7fe147101aeeb9b4547b1f4d63c68b01
soundofhorizon <49669345+soundofhorizon@users.noreply.github.com>
2020-01-18 20:29:33 +0900

    英語の表を調整

--- centermarkedmyself.php
+++ centermarkedmyself.php
@@ -1,4 +1,4 @@
-﻿<!doctype html>
+<!doctype html>
 <html lang="ja">
 <head>
   <meta charset = "utf-8">
@@ -29,7 +29,7 @@
 	<input type="submit" name = "a" value="科目選択">
 </form>
 <hr>
-<p>本当にすべて解答を入力しましたか？大丈夫でしたらOKをクリックして採点ボタンを押してください。下に採点結果が表示されます。</p>
+<p>本当にすべて解答を入力しましたか？大丈夫でしたらOKをクリックして採点ボタンを押してください。下に採点結果が表示されます。(入力情報はセキュリティ上破棄されます。)</p>
 <form method="post" action="centermarkedmyself.php">
 	②　：<input type='radio' name ='OKorNO' value='OK'>OK
 	<input type='radio' name ='OKorNO' value='NO' checked>NO
@@ -46,37 +46,37 @@ if(isset($_POST["a"])){
 
 	if($_POST["subjects"]=='日本史B'){
 		$_SESSION['subject'] = $_POST["subjects"];
-		$questionNum = array(6,6,6,6,4,8);
+		$_SESSION['questionNum'] = array(6,6,6,6,4,8);
 		$_SESSION['allQuestion'] = 36;
-		$bigQuestion = 6;
+		$_SESSION['bigQuestion'] = 6;
 		$_SESSION['answer'] = array();
 		$_SESSION['points'] = array();
 	}else if($_POST["subjects"]=='政治・経済'){
 		$_SESSION['subject'] = $_POST["subjects"];
-		$questionNum = array(5,7,7,8,5,5);
+		$_SESSION['questionNum'] = array(5,7,7,8,5,5);
 		$_SESSION['allQuestion'] = 37;
-		$bigQuestion = 6;
+		$_SESSION['bigQuestion'] = 6;
 		$_SESSION['answer'] = array();
 		$_SESSION['points'] = array();
 	}else if($_POST["subjects"]=='国語'){
 		$_SESSION['subject'] = $_POST["subjects"];
-		$questionNum = array(11,9,8,8);
+		$_SESSION['questionNum'] = array(11,9,8,7);
 		$_SESSION['allQuestion'] = 36;
-		$bigQuestion = 4;
+		$_SESSION['bigQuestion'] = 4;
 		$_SESSION['answer'] = array();
-		$_SESSION['points'] = array(2,2,2,2,2,8,8,8,8,4,4,3,3,7,8,8,8,5,5,5,5,5,5,7,7,8,8,4,4,7,7,6,7,7,8);
+		$_SESSION['points'] = array(2,2,2,2,2,8,8,8,8,4,4,3,3,7,8,8,8,5,5,5,5,5,5,7,7,8,8,4,4,7,7,6,7,7,8);//漢文部分配点変更
 	}else if($_POST["subjects"]=='英語'){
 		$_SESSION['subject'] = $_POST["subjects"];
-		$questionNum = array(7,18,6,8,5,10);
+		$_SESSION['questionNum'] = array(7,19,6,8,5,9);
 		$_SESSION['allQuestion'] = 54;
-		$bigQuestion = 6;
-		$_SESSION['answer'] = array();
-		$_SESSION['points'] = array(2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4,4,4,5,5,5,5,5,5,6,6,6,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,6,0,0,0,6);
+		$_SESSION['bigQuestion'] = 6;
+		$_SESSION['answer'] = array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+		$_SESSION['points'] = array(2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,4,4,4,5,5,5,5,5,5,6,6,6,5,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,6,6,6);
 	}else if($_POST["subjects"]=='リスニング'){
 		$_SESSION['subject'] = $_POST["subjects"];
-		$questionNum = array(6,7,6,6);
+		$_SESSION['questionNum'] = array(6,7,6,6);
 		$_SESSION['allQuestion'] = 25;
-		$bigQuestion = 4;
+		$_SESSION['bigQuestion'] = 4;
 		$_SESSION['answer'] = array(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
 		$_SESSION['points'] = array(2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2);
 	}
@@ -88,15 +88,15 @@ if(isset($_POST["a"])){
 	$html .= "<th colspan='2'>解答</th>";
 	$html .= "</tr>";
 	$html .= "<tr>";
-	for($i = 1 ; $i <= $bigQuestion ; $i++){
-		$html .= "<th rowspan= '{$questionNum[$i-1]}'>[{$i}]</th>";
-		for($j=1;$j<=$questionNum[$i-1];$j++){
+	for($i = 1 ; $i <= $_SESSION['bigQuestion'] ; $i++){
+		$html .= "<th rowspan= '{$_SESSION['questionNum'][$i-1]}'>[{$i}]</th>";
+		for($j=1;$j<=$_SESSION['questionNum'][$i-1];$j++){
 			$html .= "<th>{$tmp}</th>";
 			$html .= "<td>";
 			for($k = 1; $k <= 9; $k++){
 				$html .= "<input type='radio' name ='{$tmp}' value='{$k}'>{$k}";
 			}
-			$html .= "<input type='radio' name ='{$tmp}' value='0' checked>未選択";
+			$html .= "<input type='radio' name ='{$tmp}' value='None' checked>未選択";
 			$tmp++;
 			$html .= "</td>";
 			$html .= "</tr>";
@@ -114,11 +114,93 @@ if(isset($_POST["OKorNO"])){
 	$myPoint = 0;
 	if($_POST["OKorNO"]=='OK'){
 		for($l = 1; $l <= $_SESSION['allQuestion']; $l++){
-			if($_POST[$l]==$answer[$l-1]){
-				$myPoint += $points[$l-1];
+			if($subjects = "英語"){
+				if($l<=17){
+					if($_POST[$l]==$answer[$l-1]){
+						$myPoint += $points[$l-1];
+					}
+				}else if($l==18){
+					if($_POST[18]==$answer[17]&&$_POST[19]==$answer[18]){
+						$myPoint += $points[17];
+					}
+					$l = $l + 1;
+				}else if($l==20){
+					if($_POST[20]==$answer[19]&&$_POST[21]==$answer[20]){
+						$myPoint += $points[18];
+					}
+					$l = $l + 1 ;
+				}else if($l==22){
+					if($_POST[22]==$answer[21]&&$_POST[23]==$answer[22]){
+						$myPoint += $points[19];
+					}
+					$l = $l + 1 ;
+				}else if(24<=$l&&$l<=50){
+					if($_POST[$l]==$answer[$l-1]){
+						$myPoint += $points[$l-4];
+					}
+				}else if($l==51){
+					if($_POST[51]==$answer[50]&&$_POST[52]==$answer[51]&&$_POST[53]==$answer[52]&&$_POST[54]==$answer[53]){
+						$myPoint += $points[47];
+					}
+				}
+			}else{
+				if($_POST[$l]==$answer[$l-1]){
+					$myPoint += $points[$l-1];
+				}
 			}
 		}
-		$html = "<h2>貴方の{$subjects}の点数は{$myPoint}点です。</h2>";
+		$html = "<h2><center>貴方の{$subjects}の点数は {$myPoint} 点です。</center></h2>";
+		$tmp = 1;
+		$html .= "<center>採点詳細結果(未選択の場合[None]になってます)</center>";
+		$html .= "<table>";
+		$html .= "<tr><th colspan='6'>{$subjects}</th></tr>";
+		$html .= "<tr>";
+		$html .= "<th colspan='2'>番号</th>";
+		$html .= "<th>あなたの解答</th>";
+		$html .= "<th>正答</th>";
+		$html .= "<th>正誤</th>";
+		$html .= "<th>配点</th>";
+		$html .= "</tr>";
+		$html .= "<tr>";
+		for($i = 1 ; $i <= $_SESSION['bigQuestion'] ; $i++){
+			$html .= "<th rowspan= '{$_SESSION['questionNum'][$i-1]}'>[{$i}]</th>";
+			for($j=1;$j<=$_SESSION['questionNum'][$i-1];$j++){
+				$html .= "<th>{$tmp}</th>";
+				$html .= "<td><center>{$_POST[$tmp]}</center></td>";
+				$html .= "<th>{$answer[$tmp-1]}</th>";
+				if($_POST[$tmp]==$answer[$tmp-1]){
+					$html .= "<th>〇</th>";
+				}else{
+					$html .= "<th>×</th>";
+				}
+				if($subjects=="英語"){
+					if($tmp==18){
+						$html .= "<td rowspan='2'><center>{$points[17]}</center></td>";
+					}else if($tmp==20){
+						$html .= "<td rowspan='2'><center>{$points[18]}</center></td>";						
+					}else if($tmp==22){
+						$html .= "<td rowspan='2'><center>{$points[19]}</center></td>";
+					}else if($tmp==19||$tmp==21||$tmp==23){
+						
+					}else if(24<=$tmp&&$tmp<=50){
+						$html .= "<td><center>{$points[$tmp-4]}</center></td>";
+					}else if($tmp==51){
+						$html .= "<td rowspan='4'><center>{$points[47]}</center></td>";
+					}else if(52<=$tmp&&$tmp<=54){
+
+					}else{
+						$html .= "<td><center>{$points[$tmp-1]}</center></td>";
+					}
+				}else{
+					$html .= "<td><center>{$points[$tmp-1]}</center></td>";
+				}
+				$tmp++;
+				$html .= "</td>";
+				$html .= "</tr>";
+				$html .= "<tr>";
+			}
+		}
+		$html .= "</table>";
 		echo($html);
 		session_destroy();
 	}else{
@@ -128,6 +210,6 @@ if(isset($_POST["OKorNO"])){
 }
 ?>
 </form>
-<footer>Copyright &copy; 2020, soundofhorizon, All right reserved.</footer>
+<footer>Copyright &copy; 2020, soundofhorizon</footer>
 </body>
 </html>
